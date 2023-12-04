76bc3f4c8c6bec1e75056ab942ea389948b8585a
Vesco Claudio <claudio.vesco@gmail.com>
2023-12-04 19:36:56 +0100

    W rustdoc <3

--- day01/rs/src/lib.rs
+++ day01/rs/src/lib.rs
@@ -1,3 +1,31 @@
+//! --- Day 1: Trebuchet?! ---
+//!
+//! Something is wrong with global snow production, and you've been
+//! selected to take a look. The Elves have even given you a map; on
+//! it, they've used stars to mark the top fifty locations that are
+//! likely to be having problems.
+//!
+//! You've been doing this long enough to know that to restore snow
+//! operations, you need to check all fifty stars by December 25th.
+//!
+//! Collect stars by solving puzzles. Two puzzles will be made
+//! available on each day in the Advent calendar; the second puzzle is
+//! unlocked when you complete the first. Each puzzle grants one
+//! star. Good luck!
+//!
+//! You try to ask why they can't just use a weather machine ("not
+//! powerful enough") and where they're even sending you ("the sky")
+//! and why your map looks mostly blank ("you sure ask a lot of
+//! questions") and hang on did you just say the sky ("of course,
+//! where do you think snow comes from") when you realize that the
+//! Elves are already loading you into a trebuchet ("please hold
+//! still, we need to strap you in").
+//!
+//! As they're making the final adjustments, they discover that their
+//! calibration document (your puzzle input) has been amended by a
+//! very young Elf who was apparently just excited to show off her art
+//! skills. Consequently, the Elves are having trouble reading the
+//! values on the document.    
 use std::iter;
 
 use lazy_static::lazy_static;
@@ -59,6 +87,35 @@ fn match_number(r: &[u8]) -> Option<(u32, usize)> {
     None
 }
 
+/// The newly-improved calibration document consists of lines of text;
+/// each line originally contained a specific calibration value that
+/// the Elves now need to recover. On each line, the calibration value
+/// can be found by combining the first digit and the last digit (in
+/// that order) to form a single two-digit number.
+///
+/// # Examples
+/// ```raw
+/// 1abc2
+/// pqr3stu8vwx
+/// a1b2c3d4e5f
+/// treb7uchet
+/// ```
+///
+/// In this example, the calibration values of these four lines are
+/// 12, 38, 15, and 77. Adding these together produces 142.
+///
+/// Consider your entire calibration document.
+///
+/// ```rust
+/// use day01::solve_1;
+///
+/// let input = r#"1abc2
+/// pqr3stu8vwx
+/// a1b2c3d4e5f
+/// treb7uchet"#;
+///
+/// assert_eq!(solve_1(&input), 142);
+/// ```
 pub fn solve_1(input: &str) -> u32 {
     input
         .lines()
@@ -73,6 +130,40 @@ pub fn solve_1(input: &str) -> u32 {
         .sum()
 }
 
+/// Your calculation isn't quite right. It looks like some of the
+/// digits are actually spelled out with letters: one, two, three,
+/// four, five, six, seven, eight, and nine also count as valid
+/// "digits".
+///
+/// Equipped with this new information, you now need to find the real
+/// first and last digit on each line.
+///
+/// # Example
+/// ```raw
+/// two1nine
+/// eightwothree
+/// abcone2threexyz
+/// xtwone3four
+/// 4nineeightseven2
+/// zoneight234
+/// 7pqrstsixteen
+/// ```
+///
+/// In this example, the calibration values are 29, 83, 13, 24, 42,
+/// 14, and 76. Adding these together produces 281.
+/// ```rust
+/// use day01::solve_2;
+///
+/// let input = r#"two1nine
+/// eightwothree
+/// abcone2threexyz
+/// xtwone3four
+/// 4nineeightseven2
+/// zoneight234
+/// 7pqrstsixteen"#;
+///
+/// assert_eq!(solve_2(&input), 281);
+/// ```
 pub fn solve_2(input: &str) -> u32 {
     input
         .lines()
